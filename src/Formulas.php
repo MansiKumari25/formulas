59d34542645ea9807d47c9ac59205d02e40eb39a
MansiKumari25 <mansikumari@cedcoss.com>
2022-06-01 09:59:35 +0530

    Formuals added

---
+++ src/Formulas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Maths;
+
+class Formulas
+{
+    public function sum($a, $b){
+        return $a + $b;
+    }
+
+    public function difference($a, $b){
+        return $a - $b;
+    }
+}
